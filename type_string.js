7cad4608503825c48de42bebe4ec2b076b6c775d
GabrielStutz <gabrielstutz100@gmail.com>
2022-10-30 03:59:30 -0300

    Códigos aula 1

---
+++ type_string.js
@@ -0,0 +1,14 @@
+const texto_1 = "Olá, mundo!";
+const texto_2 = 'Olá, mundo';
+const senha = "senhaSuperSegura456!";
+const string_numeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+console.log(citacao);
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string OU template literal
